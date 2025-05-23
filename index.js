3e3a2ebe3825b57f4dcea07d7c60b5bcadef4cca
jesusking2722 <jesusking2722@gmail.com>
2025-05-23 19:22:30 +0900

    feat: set limit 2$

--- index.js
+++ index.js
@@ -9,7 +9,94 @@ const TelegramBot = require("node-telegram-bot-api");
 // Initialize the bot with polling
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
+<<<<<<< HEAD
+let flag = true;
+
+let totalEthTxCount = 0;
+let totalBscTxCount = 0;
+let totalBaseTxCount = 0;
+let totalPolTxCount = 0;
+
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const FILE_ID = process.env.FILE_ID;
+
+const setTotalCounts = (eth, bsc, base, pol) => {
+  totalEthTxCount = eth.length;
+  totalBscTxCount = bsc.length;
+  totalBaseTxCount = base.length;
+  totalPolTxCount = pol.length;
+};
+
+const sendMessage = async (
+  totalTxCount,
+  transactions,
+  coinPrice,
+  currentChrlePrice,
+  nextChrlePrice,
+  totalSoldTokens,
+  totalSoldTokenUSD,
+  totalUsers,
+  totalSolanaUsers,
+  type
+) => {
+  for (let i = 0; i < transactions.length - totalTxCount; i++) {
+    const value = weiToUsd(transactions[i].value, coinPrice);
+
+    if (parseFloat(value) >= 2) {
+      const boughtMessage = `🔥 *NEW BUY ON ${
+        type === "ETH"
+          ? "ETH"
+          : type === "BSC"
+          ? "BNB"
+          : type === "BASE"
+          ? "BASE"
+          : type === "POL"
+          ? "POLYGON"
+          : ""
+      }* 🔥\n*$CHRLE:* ${parseFloat(value / currentChrlePrice).toFixed(
+        2
+      )}\n$*Dollars:* $${parseFloat(value).toFixed(2)}\n`;
+
+      const completedMessage = `🚀 *BUY $CHRLE* 🚀\n*🌐 On Multichain:*  BNB, ETH, POLYGON, BASE, SOL, TON 🌐\n${boughtMessage}━━━━━━━━━━━━━━━━━━━━━\n💲 *Total Tokens Sold:*  ${totalSoldTokens}\n💰 *Amount Sold:*  $${totalSoldTokenUSD}\n🏷 *Current Price Per Token:*  $${currentChrlePrice}\n🏷 *Next Price Per Token:*  $${nextChrlePrice}\n📈 *Total To Raise:*  $19 830 000\n👥 *Total Holders:*  ${Number(
+        totalUsers + totalSolanaUsers
+      )}\n━━━━━━━━━━━━━━━━━━━━━\n🔗 *Explore on Blockchain:\n*🔍*View on explorer*\n🌐(${
+        type === "ETH"
+          ? "https://etherscan.io"
+          : type === "BSC"
+          ? "https://bscscan.com"
+          : type === "BASE"
+          ? "https://basescan.org"
+          : type === "POL"
+          ? "https://polygonscan.com"
+          : ""
+      }/tx/${transactions[i].hash})\n🎯 *Live explore on blockchain* 🎯`;
+
+      await bot.sendVideo(CHANNEL_ID, FILE_ID, {
+        caption: completedMessage,
+        parse_mode: "Markdown",
+        disable_web_page_preview: true,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Buy $CHRLE",
+                url: "https://charlieunicornai-sale.eu",
+              },
+            ],
+          ],
+        },
+      });
+      console.log("✅ Purchase detected: Notification sent.");
+    } else {
+      console.log("🚫 Purchase less than 2$ : Notification not sent.");
+    }
+  }
+};
+
+const scan = async () => {
+=======
 const checkPresaleUpdates = async () => {
+>>>>>>> 700780f41b7e88f8c7058f5fe6ba1687fb0d6045
   try {
     // Fetch for Solana transactions
     const {
